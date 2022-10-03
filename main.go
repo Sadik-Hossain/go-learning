c792619ddfc86cb9af5c34ee17d9eea28d3a9c2b
sadik <po480373@gmai.com>
2022-10-03 14:38:43 +0600

    escape sequence

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
+// println function fmt package er
 import "fmt"
 
+// to run go prog. -> go run <filename>.go
+// always main func. theke prog run kora shuru hobe
+// func main() {
+// 	fmt.Print("Hi, Im sadik")          //shudhu print new line create kore na,
+// 	fmt.Println("sadik learning go!!") //println new line create kore
+
+// }
+
+// Tokens of go lang
+/*
+- keywords
+- data types
+- variables
+- escape sequences
+- operators
+*/
+//* ------------- escape\ sequence -------------------------
 func main() {
-	// fmt.Printf("Hello world")
-	//string
-	var nameOne string = "mario"
-	var nameTwo = "luigi"
-	var nameThree string
-	// fmt.Println(nameOne, nameTwo, nameThree)
-	nameTwo = "25"
-	nameThree = "Peach"
-	fmt.Println(nameOne, nameTwo, nameThree)
-	//* shorthand
-	nameour := "yoshi"
-	fmt.Println(nameour)
+	fmt.Println("A \nB")             //* \n prints in new line
+	fmt.Println("Apple \t Banana")   //* \t prints tab-spaces(4 spaces)
+	fmt.Println("sadik \\ .com")     //* to print "\" we need \\
+	fmt.Println("\"sadik hossain\"") //* to print along with "" use (\") before and after str
+
 }
