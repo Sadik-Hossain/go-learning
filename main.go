e75985c267c94d68c09e069f2cdc7ddda631177a
sadik <po480373@gmai.com>
2022-10-03 14:14:23 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Printf("Hello world")
+	//string
+	var nameOne string = "mario"
+	var nameTwo = "luigi"
+	var nameThree string
+	// fmt.Println(nameOne, nameTwo, nameThree)
+	nameTwo = "25"
+	nameThree = "Peach"
+	fmt.Println(nameOne, nameTwo, nameThree)
+	//* shorthand
+	nameour := "yoshi"
+	fmt.Println(nameour)
+}
